d8db9d887c76fc5eb8d2cb9b9f2d84c8a0a82017
Esurio <esurio@esurio1673.net>
2024-12-11 18:51:49 +0900

    コメントを追加

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,17 @@ use std::process;
 
 #[derive(Parser)]
 struct Cli {
+    // 入力画像のパス
     input: String,
+    // 出力画像のパス
     output: String,
 }
 
 fn main() {
+    // CLIの引数を解析
     let args = Cli::parse();
 
+    // 画像を読み込む
     let img = match image::open(&args.input) {
         Ok(img) => img,
         Err(e) => {
@@ -20,8 +24,10 @@ fn main() {
         }
     };
 
+    // 出力先のパス
     let output_path = Path::new(&args.output);
 
+    // 画像を保存
     if let Err(e) = img.save_with_format(&output_path, ImageFormat::Png) {
         eprintln!("Error: {}", e);
         process::exit(1);
